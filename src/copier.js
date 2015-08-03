5c2a062af836ed2dee663f24871af4292bb3e971
Taylor Brennan <taylor.brennan@appdirect.com>
2015-08-02 18:54:56 -0700

    add crx; move source files to src

--- src/copier.js
+++ src/copier.js
@@ -3,7 +3,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   var textArea = document.createElement('textarea');
   document.body.appendChild(textArea);
   textArea.value = strippedUrl;
-  textArea.focus()
+  textArea.focus();
   textArea.select();
   document.execCommand('copy');
   textArea.remove();
